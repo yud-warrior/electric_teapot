5b1cfb4daedacc75fc0a6f15b87c9b67a93cb39c
yud-warrior <rodionyudenko@gmail.com>
2023-02-16 04:31:25 +0300

    fill_with_water and turn_on to teapot.py

--- src/teapot/teapot.py
+++ src/teapot/teapot.py
@@ -1,7 +1,8 @@
 from enum import Enum
 
 
-from teapot.liquid import Liquid
+from teapot.liquid import Liquid, Water
+from teapot.heater import Heater
 
 
 class TeapotState(Enum):
@@ -23,3 +24,15 @@ class Teapot:
         self.rel_volume: float = 0.
         self.temperature: float = Teapot.INITIAL_TEMPERATURE
         self.liquid: Liquid = None
+
+    def fill_with_water(self, rel_volume: float) -> None:
+        if rel_volume < 0 or rel_volume > 1:
+            raise ValueError
+        self.rel_volume = rel_volume
+        volume = rel_volume * Teapot.VOLUME
+        self.liquid = Water(volume, Teapot.INITIAL_TEMPERATURE)
+
+    def turn_on(self):
+        self.state = TeapotState.ON
+        heater = Heater(self.liquid, Teapot.POWER)
+        heater.turn_on()
